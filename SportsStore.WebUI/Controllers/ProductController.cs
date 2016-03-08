abd7b76355c29e5b0e12bb549fe48baf5222ccde
ronaldtrang <ronaldtrang@gmail.com>
2016-03-07 22:47:11 -0800

    Building the Shopping Cart

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,21 +5,46 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(string category, int page = 1)
         {
-            return View(repository.Products);
+            // Before adding pagination
+            //return View(repository.Products
+            //    .OrderBy(p => p.ProductID)
+            //    .Skip((page - 1) * PageSize)
+            //    .Take(PageSize));
+
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .Where(p => category == null || p.Category == category)
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = category == null ?
+                    repository.Products.Count() :
+                    repository.Products.Where(e => e.Category == category).Count()
+                },
+                CurrentCategory = category
+            };
+            return View(model);
         }
     }
 }
--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
@@ -31,14 +32,17 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-                new Product { Name = "Football", Price = 25},
-                new Product { Name = "Surf board", Price = 179},
-                new Product { Name = "Running shoes", Price = 95}
-            });
+            // Before the database was implemented a mock was used instead
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product> {
+            //    new Product { Name = "Football", Price = 25},
+            //    new Product { Name = "Surf board", Price = 179},
+            //    new Product { Name = "Running shoes", Price = 95}
+            //});
 
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);               
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);        
+
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
 }
